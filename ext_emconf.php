12b0c99bef9573fec33448af83f0bae9b27b9d6a
Steffen Kroggel <kroggel@rkw.de>
2024-04-19 16:33:51 +0200

    [CHANGE] dropping support for TYPO3 v9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,8 +23,8 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '9.5.3',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '9.5.0-10.4.99',
-            'core_extended' => '9.5.4-10.4.99',
+			'typo3' => '10.4.0-10.4.99',
+            'core_extended' => '10.4.0-12.4.99',
         ],
 	],
 ];
